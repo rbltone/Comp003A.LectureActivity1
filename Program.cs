a0a2d41de8805b6fa58a8e77a6a88f107cd015ee
anthonyperez5 <anthonyperez5@LEM231-23>
2023-01-18 18:56:13 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// this is a single line comment
+/* this is a milti line comment
+ * Author: Anthony Perez
+ * Course: Comp-003A
+ * Purpose: Lecture activity for this week
+ */ 
+
+namespace Comp003A.LectureActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("this is a string");
+            Console.WriteLine("Anthony Perez");
+        }
+    }
+}
